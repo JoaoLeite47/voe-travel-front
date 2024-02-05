97c21408e23506724d25ef11547862c8b289c484
JOAO_LEITE <joao.lsouza@agu.gov.br>
2024-02-05 16:22:30 -0300

    feat<home> second page

---
+++ src/components/SecondPage/SecondPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./SecondPage.scss";
+export default function SecondPage() {
+  return (
+    <div className="second-page-container">
+      <div className="second-box">
+        <label className="second-label">Solicite sua Cotação!</label>
+        <span className="second-span">
+          Confira agora a cotação para os destinos mais procurados do mundo!
+        </span>
+        <button className="second-button">Preencher Formulário!</button>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import FirstPage from "../../components/FirstPage/FirstPage";
+import SecondPage from "../../components/SecondPage/SecondPage";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <FirstPage />
+      <SecondPage />
     </div>
   );
 }
